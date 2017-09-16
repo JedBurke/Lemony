4ce4977d8ed8de4900c7bef342194191970b9db2
Jed Burke <iamthefnx@gmail.com>
2017-09-16 02:29:56 +0200

    Use semantic versioning

--- rename-py.py
+++ rename-py.py
@@ -9,10 +9,13 @@ from pathlib import Path
 import re
 import sys
 
-VERSION = "0.1"
+VERSION = "0.2.0"
 PATH_SEPARATOR = ";"
 
+# Prints certain values which the general user wouldn't need.
 debug = False
+
+# If "True", dictates that the rename action is not taken.
 dry_run = False
 
 # Profiles act as a convenient way to repetitively replace text. This 
@@ -35,96 +38,82 @@ parser.add_argument("-p", "--profile", default=None)
 args = vars(parser.parse_args())
 
 if args["debug"]:
-	debug = True
+  debug = True
 
 if debug:
-	print(args["args"])
+  print(args["args"])
 
 if args["dry_run"]:
-	dry_run = True
+  dry_run = True
 
-	if debug:
-		print(f"Dry Run: {dry_run}")
+  if debug:
+    print(f"Dry Run: {dry_run}")
 
 if args["version"]:
-	print(f"Pyren version: {VERSION}")
+  print(f"Pyren version: {VERSION}")
 
 if args["match_pattern"] is not None:
-	regex_pattern = args["match_pattern"]
+  regex_pattern = args["match_pattern"]
 
-	if debug:
-		print(f"Match Pattern: {regex_pattern}")
+  if debug:
+    print(f"Match Pattern: {regex_pattern}")
 
 if args["replace_pattern"] is not None:
-	regex_replace = args["replace_pattern"]
-	
-	if debug:
-		print(f"Replace Pattern: {regex_replace}")
+  regex_replace = args["replace_pattern"]
+  
+  if debug:
+    print(f"Replace Pattern: {regex_replace}")
 
 if args["profile"] is not None:
-	script_path = path.join(sys.path[0], "rename-profiles.json")
+  script_path = path.join(sys.path[0], "rename-profiles.json")
 
-	with open(script_path, "r") as profile_content_file:
-		profile_content = profile_content_file.read()
+  with open(script_path, "r") as profile_content_file:
+    profile_content = profile_content_file.read()
 
-	profiles = json.loads(profile_content)
-	profile_str = args["profile"]
+  profiles = json.loads(profile_content)
+  profile_str = args["profile"]
 
-	if not profile_str in profiles:
-		print(f"Profile \"{profile_str}\" not found")
-		print("Aborting.")
-		exit()
+  if not profile_str in profiles:
+    print(f"Profile \"{profile_str}\" not found")
+    print("Aborting.")
+    exit()
 
-	profile = profiles[profile_str]
-	regex_pattern = profile["match"]
-	regex_replace = profile["replace"]
-	file_types = profile["ext"]
+  profile = profiles[profile_str]
+  regex_pattern = profile["match"]
+  regex_replace = profile["replace"]
+  file_types = profile["ext"]
 
-	if debug:
-		print(f"Name: \"{profile_str}\"")
-		print(f"Match: \"{regex_pattern}\"")
-		print(f"Replace: \"{regex_replace}\"")
-		print(f"Available extensions: \"{file_types}\"")
-
-	exit()
-
-sub_ext = ["*.srt", "*.ass", "*.mkv", "*.mp4"]
-
-arg_count = len(argv)
-
-if arg_count < 2:
-	print("At least one directory must be specified. Exiting…")
-	exit()
+  if debug:
+    print(f"Name: \"{profile_str}\"")
+    print(f"Match: \"{regex_pattern}\"")
+    print(f"Replace: \"{regex_replace}\"")
+    print(f"Available extensions: \"{file_types}\"")
 
 for directory in args["args"].split(PATH_SEPARATOR):
-	print(f"Entering \"{directory}\"")
+  print(f"Entering \"{directory}\".")
 
-	p = Path(directory)
+  p = Path(directory)
 
-	if not p.is_dir():
-		print(f"Argument is not a directory")
-		continue
+  if not p.is_dir():
+    print("Skipping, not a directory.")
+    continue
 
-	# for ext in sub_ext:
-	# 	glob.glob(directory.extend(ext))
-	#  = glob.glob(directory + "\*.+(srt|ass)")
-	# for subFile in subFiles:
-	# 	print(subFile)
+  files = []
+  for ext in file_types:
+    files.extend(glob(join(directory, ext)))
 
-	sub_files = []
-	for ext in sub_ext:
-		sub_files.extend(glob(join(directory, ext)))
+  for file in files:
+    file_name = Path(file).name
 
-	# print(sub_files)
-	for file in sub_files:
-		file_name = Path(file).name
+    new_name = re.sub(regex_pattern, regex_replace, file_name)
+    new_path = join(directory, new_name)
 
-		if debug:
-			print(join(directory, re.sub(regex_pattern, regex_replace, file_name)))
+    if debug:
+      print(f"{file_name} -> {new_name}")
 
-		# m = re.match(regex_pattern, file_name)
-		# if m is not None:
-		# 	print(m.group(1) + " " + m.group(2) + m.group(3))
-
-		# else:
-		# 	print("No match")
+    # Don't perform the rename if it's a dry run.
+    if not dry_run:
+      if not os.path.exists(new_path):
+        os.rename(file, new_path)        
+      else:
+        print(f"Skipping, a file with the name of \"{new_name}\" already exists.")
