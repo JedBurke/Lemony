e5ad41c730a467caf71771bd429e1c6614bf7c70
Jed Burke <iamthefnx@gmail.com>
2017-12-16 00:08:04 +0200

    Begin DirectoryManager rework

--- modules/directory_manager.py
+++ modules/directory_manager.py
@@ -1,26 +1,23 @@
-import unittest
-import logging
-from glob import glob
-from pathlib import Path
+from .pathobject_manager import PathObjectManager
 
-class DirectoryManager(object):
-    """
-    Represents a class which stores a list of directories while validating
-    which is valid and may be added.
-    """
-    def __init__(self):
-        self.__directories = []
-        super(
-            DirectoryManager,
-            self
-        ).__init__()
-    
+from logging import logger
+from pathlib import path
+
+class FileManager(PathObjectManager):
+    def __init__(self):        
+        super().__init__()
+
+        # self.whitelist = True
+        self.validation_function = self.is_valid_directory
+
     """
     Determines whether the path is a valid directory which may be added 
     to the the current instance.
-    Returns: Boolean
+    
+    Returns:
+        Boolean
     """
-    def is_valid_directory(self, directory, directory_list):
+    def is_valid_directory(self, directory):
         if self.is_empty(directory) or self.exists(directory):
            return False
 
@@ -33,7 +30,9 @@ class DirectoryManager(object):
 
     """
     Determines whether the path is logically empty.
-    Returns: Boolean
+    
+    Returns:
+        Boolean
     """
     def is_empty(self, string):
         if string == "" or string == None:
@@ -41,82 +40,4 @@ class DirectoryManager(object):
             return True
 
         else:
-            return False        
-
-    """
-    Replaces the Windows-specific path separator with the Unix-like one.
-    Returns: String of the normalized path.
-    """
-    def normalize_path(self, path):
-        return path.replace("\\", "/")
-
-    """
-    Adds a directory to the directory manager instance.
-    """
-    def add(self, directory):
-        directory_list = []
-
-        if isinstance(directory, list):
-            directory_list.extend(directory)
-
-        else:
-            directory_list.append(directory)
-
-        for path in directory_list:
-            for globbed_path in glob(path):
-                globbed_path = self.normalize_path(globbed_path)
-
-                if self.is_valid_directory(globbed_path, self.__directories):
-                    self.__directories.append(globbed_path)
-
-    """
-    Removes the specified path from the instance.
-    Returns: A boolean of whether all specified paths were removed.
-    """
-    def remove(self, directory):
-        raise NotImplementedError
-
-    """
-    Completely clears the instance.
-    Returns: A boolean if it was successful.
-    """
-    def clear(self):
-        self.__directories.clear()
-        return True
-
-    """
-    Returns a list of all the directories managed by the instance.
-    Returns: List
-    """
-    def list(self):
-        return list(self.__directories)
-
-    def exists(self, directory):
-        if directory in self.__directories:
-            logging.info(f"Directory exists: { directory }")
-            return True
-        
-        else:
             return False
-
-
-# class TestDirectoryManager(object):
-#     """docstring for TestDirectoryManager"""
-
-#     self.manager = None
-#     def setUp(self):
-#         self.manager = DirectoryManager()
-
-#     def test_add_directories(self):        
-#         self.manager.add([
-#             "d://evrnet",
-#             "c://byteb",
-#             "c://hello",
-#             "c://ruby/*",
-#             "c://scripts/*"
-#         ])
-
-#     def tearDown(self):
-#         self.manager.clear();
-#         self.manager = None
-        
